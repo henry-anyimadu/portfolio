fff5c14ac391773005c508916d2e473710f6587c
Henry Anyimadu <henry@whatsfabric.com>
2025-01-07 15:56:42 -0600

    updated config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,20 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '');
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  // Required for GitHub Pages
-  basePath: "",
-  assetPrefix: "https://henryany.com",
+  // Correctly handle asset prefixes for both development and production
+  basePath: isGithubActions ? `/${repo}` : '',
+  assetPrefix: isGithubActions ? `/${repo}/` : '',
+  // Enable static file tracing
+  experimental: {
+    forceSwcTransforms: true,
+  },
 };
 
 export default nextConfig;
