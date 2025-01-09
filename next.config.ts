d96bee9a82f603b05a1db32cec6a93082f7ac160
Henry Anyimadu <henry@whatsfabric.com>
2025-01-09 17:42:38 -0600

    changed next config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,13 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '');
 
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  // Correctly handle asset prefixes for both development and production
-  basePath: isGithubActions ? `/${repo}` : '',
-  assetPrefix: isGithubActions ? `/${repo}/` : '',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
 };
 
 export default nextConfig;
