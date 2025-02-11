afc191782ff317229bd01cdada69c6b3cfae8e33
Henry Anyimadu <henry@whatsfabric.com>
2025-02-10 22:51:25 -0600

    Added site analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from './components/theme-provider'
 import ThemeToggle from './components/theme-toggle'
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -63,6 +64,7 @@ export default function RootLayout({
           type="font/woff2"
           crossOrigin="anonymous"
         />
+          <Analytics />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
